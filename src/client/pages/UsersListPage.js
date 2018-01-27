c8558cdfbdcb0deb479fd154b16ba9d2a9612cdf
hluong <hluong@AdminUsers-MacBook-Pro.local>
2018-01-26 16:13:29 -0800

    add requireAuth && SEO meta tag

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchUsers } from '../actions';
+import { Helmet } from 'react-helmet';
 
 class UsersList extends Component {
     componentDidMount() {
@@ -12,10 +13,18 @@ class UsersList extends Component {
             return <li key={user.id}>{user.name}</li>
         })
     }
-
+    head() {
+        return (
+            <Helmet>
+                <title>{`${this.props.users.length} Users Loaded`}</title>
+                <meta property="og:title" content="Users App"/>
+            </Helmet>
+        )
+    }
     render() {
         return (
             <div>
+                {this.head()}
                 List users:
                 <ul>{this.renderUsers()}</ul>
             </div>
